a47766436ef2c3b68e1373bb51cce4128a487af1
Bert.Gibbons <bgibbons@udel.edu>
2017-09-26 14:39:04 -0400

    Ready for Lab #3...

--- FlamingoBLL/src/main/java/pkgCore/HandPoker.java
+++ FlamingoBLL/src/main/java/pkgCore/HandPoker.java
@@ -10,58 +10,70 @@ public class HandPoker extends Hand {
 	
 	@Override
 	public HandScore ScoreHand() {
+		//TODO : Implement this method...  call each of the 'is' methods (isRoyalFlush, etc) until
+		//		one of the hands is true, then score the hand
 		return null;
 	}
 	
 	public boolean isRoyalFlush()
 	{
 		boolean bIsRoyalFlush = false;
+		//TODO : Implement this method
 		return bIsRoyalFlush;
 	}
 	public boolean isStraightFlush()
 	{
 		boolean bisStraightFlush = false;
+		//TODO : Implement this method
 		return bisStraightFlush;
 	}
 	public boolean isFourOfAKind()
 	{
 		boolean bisFourOfAKind = false;
+		//TODO : Implement this method
 		return bisFourOfAKind;
 	}
 	public boolean isFullHouse()
 	{
 		boolean bisFullHouse = false;
+		//TODO : Implement this method
 		return bisFullHouse;
 	}
 	public boolean isFlush()
 	{
 		boolean bisFlush = false;
+		//TODO : Implement this method
 		return bisFlush;
 	}
 
 	public boolean isStraight()
 	{
 		boolean bisStraight = false;
+		//TODO : Implement this method
 		return bisStraight;
 	}
 	public boolean isThreeOfAKind()
 	{
 		boolean bisThreeOfAKind = false;
+		//TODO : Implement this method
 		return bisThreeOfAKind;
 	}
 	public boolean isTwoPair()
 	{
 		boolean bisTwoPair = false;
+		//TODO : Implement this method
 		return bisTwoPair;
 	}
 	public boolean isPair()
 	{
 		boolean bisPair = false;
+		//TODO : Implement this method
 		return bisPair;
 	}
 	public boolean isHighCard()
 	{
 		boolean bisHighCard = false;
+		//TODO : Implement this method
 		return bisHighCard;
 	}	
 	
--- FlamingoBLL/src/main/java/pkgCore/HandScorePoker.java
+++ FlamingoBLL/src/main/java/pkgCore/HandScorePoker.java
@@ -1,12 +1,21 @@
 package pkgCore;
 
 import java.util.ArrayList;
+import pkgEnum.eHandStrength;
 
 public class HandScorePoker extends HandScore {
+	private eHandStrength eHandStrength;
 	private Card HiCard;
 	private Card LoCard;
 	private ArrayList<Card> kickers = new ArrayList<Card>();
 
+	
+	protected eHandStrength geteHandStrength() {
+		return eHandStrength;
+	}
+	protected void seteHandStrength(eHandStrength eHandStrength) {
+		this.eHandStrength = eHandStrength;
+	}
 	protected Card getHiCard() {
 		return HiCard;
 	}
